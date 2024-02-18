310c8e477a75f55f5ee490fef7d47b17c882d671
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-18 19:38:17 -0300

    feat: Adiciona layout do site com navegação

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Navigation } from '@/components/site/navigation'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="h-full">
+      <Navigation />
+      {children}
+    </main>
+  )
+}
